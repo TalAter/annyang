3e1c626a6c5a3d4bd25934f42cc82a2a9a391984
Tal Ater <tal@talater.com>
2013-08-23 15:51:25 +0300

    Fleshed out recognition.onresult() method to retrieve the command text. Added maxAlternatives setting.

--- voxcom.js
+++ voxcom.js
@@ -15,17 +15,23 @@
   }
 
   var recognition = new webkitSpeechRecognition();
+  recognition.maxAlternatives = 5;
   recognition.continuous = true;
   /* @TODO: Add language support */
   recognition.lang = "en";
 
   recognition.onstart   = function()      { /* @TODO: Show visual cue that voice recognition is happening */ };
 
-  recognition.onresult  = function(event) { /* @TODO: Take action */ };
+  recognition.onresult  = function(event) {
+    var results = event.results[event.resultIndex];
+    var commandText = results[0].transcript.trim();
+    // // @TODO: If not too confident about text, consider using the alternatives provided by API or ask for user intervention.
+    window.console.log(commandText);
+  };
 
-  recognition.onerror   = function(event) { /* @TODO: handle errors */ };
+  recognition.onerror   = function()      { /* @TODO: handle errors */ };
 
-  recognition.onend     = function()      {};
+  recognition.onend     = function()      { /* @TODO: restart speech recognition */ };
 
   /* @TODO: Check permission to use the mic, and alert user to permission problem */
   recognition.start();
