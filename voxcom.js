587e6827a88a3fb72733a786e1f312018640810c
Tal Ater <tal@talater.com>
2013-08-23 19:46:03 +0300

    Commands now support params and splats, through the magic of regex.

--- voxcom.js
+++ voxcom.js
@@ -18,6 +18,22 @@
     return null;
   }
 
+  // The command matching code is based on Backbone.Router by Jeremy Ashkenas, under the MIT license.
+  var optionalParam = /\((.*?)\)/g;
+  var namedParam    = /(\(\?)?:\w+/g;
+  var splatParam    = /\*\w+/g;
+  var escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+  var commandToRegExp = function(command) {
+    command = command.replace(escapeRegExp, '\\$&')
+                 .replace(optionalParam, '(?:$1)?')
+                 .replace(namedParam, function(match, optional) {
+                   return optional ? match : '([^\\s]+)';
+                 })
+                 .replace(splatParam, '(.*?)');
+    return new RegExp('^' + command + '$', 'i');
+  };
+
+  // initiate webkitSpeechRecognition
   var recognition = new webkitSpeechRecognition();
   recognition.maxAlternatives = 5;
   recognition.continuous = true;
@@ -34,13 +50,13 @@
     var results = event.results[event.resultIndex];
     var commandText;
     for (var i = 0; i<results.length; i++) {
-      commandText = results[i].transcript.trim().toLowerCase();
+      commandText = results[i].transcript.trim();
+      window.console.log('recognized: '+commandText);
 
-      window.console.log(commandText);
       for (var j in commandsList) {
-        // @TODO: Refactor commands to regular expressions
-        if (commandsList[j].command === commandText) {
-          commandsList[j].callback.apply();
+        var result = commandsList[j].command.exec(commandText);
+        if (result) {
+          commandsList[j].callback.apply(this, result.slice(1));
           return true;
         }
       }
@@ -52,14 +68,18 @@
   root.voxcom = {
     init: function(commands) {
       commandsList = [];
-      var cb;
+      var cb,
+          command;
       for (var i in commands) {
         cb = root[commands[i]] || commands[i];
         if (typeof cb !== 'function') {
           continue;
         }
+        //convert command to regex
+        command = commandToRegExp(i);
+
         //@TODO: Check if there are issues with context for the callback
-        commandsList.push({ command: i.toLowerCase(), callback: cb });
+        commandsList.push({ command: command, callback: cb });
       }
     },
 
