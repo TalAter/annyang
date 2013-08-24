8096f70a7e78b75af8fddb008fec9ba0394f3cbf
Tal Ater <tal@talater.com>
2013-08-24 03:12:28 +0300

    Added sample site file: facebook

---
+++ sites/facebook.js
@@ -0,0 +1,123 @@
+// VoxCom - facebook.com
+// version : 0.0.1
+// author : Tal Ater
+// license : GNU v2
+// TalAter.com
+
+(function () {
+  /*global voxcom */
+  "use strict";
+  var root = this;
+
+  if (voxcom) {
+    var chatToggle = function() {
+      root.Chat.toggleSidebar();
+    };
+
+    var chatOnline = function() {
+      root.ChatVisibility.goOnline();
+    };
+
+    var chatOffline = function() {
+      root.ChatVisibility.goOffline();
+    };
+
+    var searchMore = function(term) {
+      root.goURI('/search/more/?q='+term);
+    };
+
+    var gotoPage = function(term) {
+      root.goURI('/'+term.replace(/[^\w]/ig, ''));
+    };
+
+    var stream = function() {
+      root.goURI('/');
+    };
+
+    var appcenter = function() {
+      root.goURI('/appcenter');
+    };
+
+    var messages = function() {
+      root.goURI('/messages');
+    };
+
+    var friendRequests = function() {
+      root.goURI('/friends/requests');
+    };
+
+    var friends = function() {
+      root.goURI('/friends');
+    };
+
+    var profile = function() {
+      root.goURI('/me');
+    };
+
+    var events = function() {
+      root.goURI('/events/list');
+    };
+
+    var calendar = function() {
+      root.goURI('/events/calendar');
+    };
+
+    var photos = function(name) {
+      root.goURI('/'+(name || 'me').replace(/[^\w]/ig, '')+'/photos');
+    };
+
+    var albums = function(name) {
+      root.goURI('/'+(name || 'me').replace(/[^\w]/ig, '')+'/photos_albums');
+    };
+
+    var goBack = function() {
+      history.back();
+    };
+
+    var pageScroll = function(direction) {
+      if (direction === 'up') {
+        window.scrollBy(0, -(window.innerHeight-$('pageHead').offsetHeight));
+      } else {
+        window.scrollBy(0, window.innerHeight-$('pageHead').offsetHeight);
+      }
+    };
+
+    voxcom.init({
+      'back':                 goBack,
+      'go back':              goBack,
+      'go home':              stream,
+      'home':                 stream,
+      'news':                 stream,
+      'profile':              profile,
+      'me':                   profile,
+      'games':                appcenter,
+      'apps':                 appcenter,
+      'messages':             messages,
+      'inbox':                messages,
+      'mail':                 messages,
+      'email':                messages,
+      'events':               events,
+      'calendar':             calendar,
+      'photos':               photos,
+      'photos *page':         photos,
+      'albums':               albums,
+      'albums *page':         albums,
+      'friend requests':      friendRequests,
+      'friends':              friends,
+      'chat':                 chatToggle,
+      'chat online':          chatOnline,
+      'go online':            chatOnline,
+      'chat offline':         chatOffline,
+      'go offline':           chatOffline,
+      'scroll':               pageScroll,
+      'scroll :direction':    pageScroll,
+      'find *term':           searchMore,
+      'search for *term':     searchMore,
+      'search *term':         searchMore,
+      'go to *page':          gotoPage,
+      'go *page':             gotoPage,
+    });
+    voxcom.start();
+  }
+
+}).call(this);
