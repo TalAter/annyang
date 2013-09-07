5c3e48f38e4c1f9eb8dd59bd0fc7ec1e40f2aed6
Tal Ater <tal@talater.com>
2013-09-08 01:39:23 +0300

    Added JSHint task and settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,48 @@
 module.exports = function(grunt) {
+  "use strict";
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'annyang.js',
+        'sites/facebook.js'
+      ],
+      options: {
+        "node"      : true,
+        "browser"   : true,
+        "devel"     : false,
+        "camelcase" : true,
+        "curly"     : true,
+        "latedef"   : true,
+        "unused"    : true,
+        "trailing"  : true,
+        "eqeqeq"    : true,
+        "eqnull"    : true,
+        "evil"      : false,
+        "forin"     : true,
+        "immed"     : true,
+        "laxbreak"  : false,
+        "newcap"    : true,
+        "noarg"     : true,
+        "noempty"   : false,
+        "nonew"     : true,
+        "onevar"    : false,
+        "plusplus"  : false,
+        "undef"     : true,
+        "sub"       : true,
+        "strict"    : true,
+        "white"     : false,
+        "indent"    : 2
+      }
+    },
     uglify: {
       options: {
         preserveComments: 'some'
       },
-      annyang: {
+      all: {
         files: {
           'annyang.min.js': ['annyang.js'],
           'sites/facebook.min.js': ['annyang.js', 'sites/facebook.js']
@@ -19,7 +54,10 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
