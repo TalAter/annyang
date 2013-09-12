9abd91b24603ff6a94e421aa26ce22f14f1aafb7
Cătălin Mariș <alrraa@gmail.com>
2013-09-12 15:58:17 +0300

    Refactor code
    * Simplify and make code more consistent / readable.
    * Remove / replace the parts of the code that do not work across
      browsers (e.g.: customizable `console.log` messages), or are
      considered bad practices (e.g.: using `void`).

--- annyang.js
+++ annyang.js
@@ -1,178 +1,212 @@
-//! annyang
-//! version : 0.2.0
-//! author  : Tal Ater @TalAter
-//! license : MIT
-//! https://www.TalAter.com/annyang/
-(function () {
+/*! annyang v0.2.0 | MIT license | https://www.talater.com/annyang/ */
+
+(function ( window, undefined ) {
+
   "use strict";
 
-  var root = this;
-  var SpeechRecognition = root.webkitSpeechRecognition ||
-                          root.mozSpeechRecognition ||
-                          root.msSpeechRecognition ||
-                          root.oSpeechRecognition ||
-                          root.SpeechRecognition;
-  var commandsList;
-  var recognition;
-  var lang = 'en-US';
-  var callbacks = { start: [], error: [], end: [], result: [], resultMatch: [], resultNoMatch: [] };
+  var annyang;
   var autoRestart;
+  var callbacks = {
+      end: [],
+      error: [],
+      result: [],
+      resultMatch: [],
+      resultNoMatch: [],
+      start: []
+    };
+  var commandsList;
   var debugState = false;
-  var debugStyle = 'font-weight: bold; color: #00f;';
+  var lang = 'en-US';
+  var recognition;
 
-  // Check browser support
-  if ( !SpeechRecognition ) {
-    root.annyang = null;
-    return null;
-  }
+  // normalize SpeechRecognition
+  var SpeechRecognition = window.webkitSpeechRecognition ||
+                          window.mozSpeechRecognition ||
+                          window.msSpeechRecognition ||
+                          window.oSpeechRecognition ||
+                          window.SpeechRecognition;
 
-  // The command matching code is a modified version of Backbone.Router by Jeremy Ashkenas, under the MIT license.
+  // The command matching code is a modified version of
+  // Backbone.Router by Jeremy Ashkenas, under the MIT license.
+  var escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#]/g;
+  var namedParam    = /(\(\?)?:\w+/g;
   var optionalParam = /\s*\((.*?)\)\s*/g;
   var optionalRegex = /(\(\?:[^)]+\))\?/g;
-  var namedParam    = /(\(\?)?:\w+/g;
   var splatParam    = /\*\w+/g;
-  var escapeRegExp  = /[\-{}\[\]+?.,\\\^$|#]/g;
-  var commandToRegExp = function(command) {
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  function abort() {
+    autoRestart = false;
+    recognition.abort();
+  }
+
+  function addCallback(type, callback) {
+    if ( callbacks[type] !== undefined &&
+        typeof cb === 'function' ) {
+      callbacks[type].push(callback);
+    }
+  }
+
+  function addCommands(commands) {
+
+    var phrase;
+
+    for ( phrase in commands ) {
+      if ( commands.hasOwnProperty(phrase) &&
+          typeof commands[phrase] === 'function') {
+        commandsList.push({
+          callback: commands[phrase],
+          command: commandToRegExp(phrase),
+          originalPhrase: phrase
+        });
+      }
+    }
+
+    if ( debugState ) {
+      console.log('Commands successfully loaded: ' + commandsList.length);
+    }
+
+  }
+
+  function commandToRegExp(command) {
     command = command.replace(escapeRegExp, '\\$&')
-                  .replace(optionalParam, '(?:$1)?')
-                  .replace(namedParam, function(match, optional) {
-                    return optional ? match : '([^\\s]+)';
-                  })
-                  .replace(splatParam, '(.*?)')
-                  .replace(optionalRegex, '\\s*$1?\\s*');
+                     .replace(optionalParam, '(?:$1)?')
+                     .replace(namedParam, function (match, optional) {
+                        return optional ? match : '([^\\s]+)';
+                      })
+                     .replace(splatParam, '(.*?)')
+                     .replace(optionalRegex, '\\s*$1?\\s*');
     return new RegExp('^' + command + '$', 'i');
-  };
+  }
 
-  var invokeCallbacks = function(callbacks) {
-    for (var j = 0, l = callbacks.length; j < l; j++) {
-      callbacks[j].apply(this);
-    }
-  };
 
-  root.annyang = {
-    init: function(commands) {
+  function debug(newState) {
+    debugState = newState === undefined ? true : newState;
+    return debugState;
+  }
 
-      // Abort previous instances of recognition already running
-      if (recognition && recognition.abort) {
-        recognition.abort();
-      }
+  function init(commands) {
 
-      // initiate
-      recognition = new SpeechRecognition();
-
-      recognition.maxAlternatives = 5;
-      recognition.continuous = true;
-      recognition.lang = lang;
-
-      recognition.onstart   = function()      { invokeCallbacks(callbacks.start); };
-
-      recognition.onerror   = function()      { invokeCallbacks(callbacks.error); };
-
-      recognition.onend     = function() {
-        invokeCallbacks(callbacks.end);
-        if (autoRestart) {
-          root.annyang.start();
-        }
-      };
-
-      recognition.onresult  = function(event) {
-        invokeCallbacks(callbacks.result);
-        var results = event.results[event.resultIndex];
-        var commandText;
-        for (var i = 0; i<results.length; i++) {
-          commandText = results[i].transcript.trim();
-          if (debugState) {
-            root.console.log('Speech recognized: %c'+commandText, debugStyle);
-          }
-
-          for (var j = 0, l = commandsList.length; j < l; j++) {
-            var result = commandsList[j].command.exec(commandText);
-            if (result) {
-              var parameters = result.slice(1);
-              if (debugState) {
-                root.console.log('command matched: %c'+commandsList[j].originalPhrase, debugStyle);
-                if (parameters.length) {
-                  root.console.log('with parameters', parameters);
-                }
-              }
-              commandsList[j].callback.apply(this, parameters);
-              invokeCallbacks(callbacks.resultMatch);
-              return true;
-            }
-          }
-        }
-        invokeCallbacks(callbacks.resultNoMatch);
-        return false;
-      };
-
-      // build commands list
-      commandsList = [];
-      this.addCommands(commands);
-    },
-
-    start: function(options) {
-      options = options || {};
-      if (typeof options.autoRestart !== 'undefined') {
-        autoRestart = !!options.autoRestart;
-      } else {
-        autoRestart = true;
-      }
-      recognition.start();
-    },
-
-    abort: function() {
-      autoRestart = false;
+    // abort any previous instances of recognition already running
+    if ( recognition ) {
       recognition.abort();
-    },
-
-    debug: function(newState) {
-      if (arguments.length > 0) {
-        debugState = !!newState;
-      } else {
-        debugState = true;
-      }
-    },
-
-    setLanguage: function(language) {
-      lang = language;
-      if (recognition && recognition.abort) {
-        recognition.lang = language;
-      }
-    },
-
-    addCommands: function(commands) {
-      var cb,
-          command;
-      for (var phrase in commands) {
-        if (commands.hasOwnProperty(phrase)) {
-          cb = root[commands[phrase]] || commands[phrase];
-          if (typeof cb !== 'function') {
-            continue;
-          }
-          //convert command to regex
-          command = commandToRegExp(phrase);
-
-          commandsList.push({ command: command, callback: cb, originalPhrase: phrase });
-        }
-      }
-      if (debugState) {
-        root.console.log('Commands successfully loaded: %c'+commandsList.length, debugStyle);
-      }
-    },
-
-    /**
-     * Lets the user add a callback of one of 6 types: start, error, end, result, resultMatch, resultNoMatch
-     */
-    addCallback: function(type, callback) {
-      if (callbacks[type]  === void 0) {
-        return;
-      }
-      var cb = root[callback] || callback;
-      if (typeof cb !== 'function') {
-        return;
-      }
-      callbacks[type].push(cb);
     }
-  };
 
-}).call(this);
+    // initiate
+    recognition = new SpeechRecognition();
+
+    recognition.continuous = true;
+    recognition.lang = lang;
+    recognition.maxAlternatives = 5;
+
+    recognition.onend = function () {
+      invokeCallbacks(callbacks.end);
+      if ( autoRestart ) {
+        annyang.start();
+      }
+    };
+
+    recognition.onerror = function () {
+      invokeCallbacks(callbacks.error);
+    };
+
+    recognition.onstart = function () {
+      invokeCallbacks(callbacks.start);
+    };
+
+    recognition.onresult = function (event) {
+
+      var commandText;
+      var i = 0;
+      var j = 0;
+      var parameters;
+      var result;
+      var results = event.results[event.resultIndex];
+
+      invokeCallbacks(callbacks.result);
+
+      for ( i = 0; i < results.length; i++ ) {
+        commandText = results[i].transcript.trim();
+
+        if (debugState) {
+          console.log('Speech recognized: ' + commandText);
+        }
+
+        for ( j = 0; j < commandsList.length; j++ ) {
+          result = commandsList[j].command.exec(commandText);
+
+          if ( result ) {
+            parameters = result.slice(1);
+            if (debugState) {
+              console.log('command matched: ' + commandsList[j].originalPhrase);
+              if (parameters.length) {
+                console.log(' with parameters ', parameters);
+              }
+            }
+            commandsList[j].callback(parameters);
+            invokeCallbacks(callbacks.resultMatch);
+            return true;
+          }
+
+        }
+
+      }
+
+      invokeCallbacks(callbacks.resultNoMatch);
+    };
+
+    // build commands list
+    commandsList = [];
+    addCommands(commands);
+  }
+
+  function invokeCallbacks(callbacks) {
+    var j = 0, l = callbacks.length;
+    for ( j = 0; j < l; j++ ) {
+      callbacks[j]();
+    }
+  }
+
+  function setLanguage(language) {
+    if ( recognition ) {
+      recognition.abort();
+    }
+    recognition.lang = language;
+  }
+
+  function start(options) {
+    if ( options && options.autoRestart ) {
+      autoRestart = !!options.autoRestart;
+    } else {
+      autoRestart = true;
+    }
+    recognition.start();
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  // check for browser support
+  if ( !SpeechRecognition ) {
+    // if there isn't support for SpeechRecognition, there is nothing we can do
+    annyang = undefined;
+
+  // if there is support
+  } else {
+
+    // create the `annyang` object
+    annyang = {
+      abort: abort,
+      addCallback: addCallback,
+      addCommands: addCommands,
+      debug: debug,
+      init: init,
+      setLanguage: setLanguage,
+      start: start
+    };
+  }
+
+  // and expose it to the global object
+  window.annyang = annyang;
+
+}( window ));
