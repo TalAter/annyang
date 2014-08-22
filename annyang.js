19a7092cc215d7a1c76d6c903fe20bb4119ff893
Tal Ater <tal@talater.com>
2014-08-22 03:51:03 +0300

    Updated API docs

--- annyang.js
+++ annyang.js
@@ -10,7 +10,7 @@
   /**
    * # Quick Tutorial, Intro and Demos
    *
-   * The quickest way to get started, is by visiting the [annyang homepage](https://www.talater.com/annyang/).
+   * The quickest way to get started is to visit the [annyang homepage](https://www.talater.com/annyang/).
    *
    * For a more in-depth look at annyang, read on.
    *
