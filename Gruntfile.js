78eecc324554704308fda18ed5899a80a885acd2
Tal Ater <tal@talater.com>
2016-01-14 12:05:55 +0200

    JSHint now goes over test specs

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
         'annyang.js',
         'sites/facebook.js',
         'sites/geektime.js',
+        'test/spec/*Spec.js'
       ],
       options: {
         jshintrc: true
